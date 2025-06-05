9ed2f8d304f29fffb7d708f9d27fda1ab7bd294d
klinga <klingaroo@gmail.com>
2025-06-04 23:58:43 -0400

    fixes unpacking zip files

--- govdocs/utils.py
+++ govdocs/utils.py
@@ -4,13 +4,21 @@ import zipfile
 
 
 def unpack_zipfiles(delivery_dir: Path) -> list[str]:
-    with zipfile.ZipFile(delivery_dir, "r") as zip_files:
-        extracted_files = zip_files.namelist()
-        zip_files.extractall(path=delivery_dir)
+    files = [
+        entry
+        for entry in delivery_dir.iterdir()
+        if entry.is_file() and entry.suffix == ".zip"
+    ]
+    extracted_files = []
+    for f in files:
+        with zipfile.ZipFile(f, "r") as zip_file:
+            extracted_file = zip_file.namelist()
+            zip_file.extractall(path=delivery_dir)
+            extracted_files.extend(extracted_file)
     return extracted_files
 
 
 def get_directory(delivery_date: str) -> Path:
-    delivery_obj = datetime.strptime(delivery_date, "%Y%m%d")
-    delivery_dir = Path.cwd() / f"files/{delivery_obj:%Y-%m-%d}"
+    delivery_date_obj = datetime.strptime(delivery_date, "%Y%m%d")
+    delivery_dir = Path.cwd() / f"files/{delivery_date_obj:%Y-%m-%d}"
     return delivery_dir
